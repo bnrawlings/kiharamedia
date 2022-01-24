c8c58296dc9f72c163393dc26f444c1c00ae904d
Brittney Rawlings <bn.rawlings@gmail.com>
2022-01-24 13:03:34 -0600

    components folders added

--- kiharamedia/kiharamedia/src/App.js
+++ kiharamedia/kiharamedia/src/App.js
@@ -1,19 +1,25 @@
-
-import './App.css';
-import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import Home from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Contact from './pages/Contact/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import { Navbar, Footer } from './components';
 
 function App() {
   return (
-    <>
     <Router>
-     <NavBar/>
-     <Routes>
-       <Route path ='/' exact/>
-     </Routes>
-     </Router>
-    </>
+      <GlobalStyle />
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact' component={Contact} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ kiharamedia/kiharamedia/src/pages/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { InfoSection } from '../../components';
+import { homeObjOne, homeObjThree } from './Data';
+function Contact() {
+    return (
+        <>
+          <InfoSection {...homeObjOne} />
+          <InfoSection {...homeObjThree} />
+        </>
+    )
+}
+
+export default Contact;

d924a5d1f2c33a8b0b88407211d8012860cf9162
Brittney Rawlings <bn.rawlings@gmail.com>
2022-03-10 10:05:17 +0300

    @ react types declarations adjusted

--- kiharamedia/kiharamedia/src/pages/IntroPage/Intro.js
+++ kiharamedia/kiharamedia/src/pages/IntroPage/Intro.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Player } from 'video-react';
+import{ KiharaMediaWebVideo.mp4 } from "../../../public/videos/KiharaMediaWebVideo.mp4"
 
-import { homeObjOne } from './Data';
-import { InfoSection} from '../../components';
+// import { homeObjOne } from './Data';
+// import { InfoSection} from '../../components';
 
 function Intro() {
   return (
     <>
-    <InfoSection {...homeObjOne} />
+    <Player 
+    playsInline
+    src= { KiharaMediaWebVideo } />
     </>
   )
 }

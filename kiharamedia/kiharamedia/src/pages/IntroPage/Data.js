471f492be63086a3fd688fe80a83f0aafa3b5849
Brittney Rawlings <bn.rawlings@gmail.com>
2022-07-15 14:00:03 +0300

    last update

---
+++ kiharamedia/kiharamedia/src/pages/IntroPage/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Marketing Agency',
+    headline: 'Lead Generation Specialist for Businesses',
+    description:
+      'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: require('../../images/photo1.jpeg'),
+    alt: 'Credit Card',
+    start: ''
+  };
